c3fc4fbd1fe2604a92d9fa1663d7016c890349df
Christopher Adams <chris@raysend.com>
2012-07-24 01:35:58 -0400

    Keep validation in UsersController index DRY

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -83,30 +83,17 @@ class UsersController extends \lithium\action\Controller {
         	return $this->redirect('Users::index');
         }
         
-        //TODO don't repeat the entire validation from the Users model
-        //instead, just add the uniqueUsername rule when creating a new user
-        $validates = array(
-			'username' => array(
-				array('uniqueUsername', 'message' => 'This username is already taken.'),
-				array('notEmpty', 'message' => 'Please enter a username.'),
-				array('alphaNumeric', 'skipEmpty' => true, 'message' => 'Alphanumeric characters only.'),
-			),
-		    'password' => array(
-		        array('notEmpty', 'message'=>'Please enter a password.')
-		    ),
-		    'name' => array(
-		        array('notEmpty', 'message'=>'Please enter a full name.')
-		    ),
-		    'email' => array(
-		        array('notEmpty', 'message'=>'Include an email address.'),
-		        array('email', 'skipEmpty' => true, 'message' => 'The email address must be valid.')
-		    ),
-		);
+        //Get the validation rules from the Model
+        $userModel = new Users();
+        $rules = $userModel->validates;
+        
+        //Add the uniqueUsername validation rule
+        array_push(&$rules['username'], array('uniqueUsername', 'message' => 'This username is already taken.'));
 		
 		$user = Users::create();
 		$roles = Roles::all();
 
-		if (($this->request->data) && $user->save($this->request->data, array('validate' => $validates))) {
+		if (($this->request->data) && $user->save($this->request->data, array('validate' => $rules))) {
 			return $this->redirect(array('Users::view', 'args' => array($user->username)));
 		}
 		return compact('user', 'roles');
