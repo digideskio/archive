df1dcfd7885bb48fc5eb0ec5d0ca2f7bfcc28ba3
Christopher Adams <chris@raysend.com>
2012-08-02 02:18:14 -0400

    Highlight current controller in sidebar

--- app/views/elements/sidebar.html.php
+++ app/views/elements/sidebar.html.php
@@ -1,16 +1,25 @@
+<?php $c = $this->request()->params['controller']; ?>
+
 <div id="sidebar"  class="span2">
 	
 	<div class="well" style="padding: 8px 0;">
 		<ul class="nav nav-list">
-			<li><a href="/collections"><i class="icon-briefcase"></i> Collections</a></li>
-			<li><a href="/works"><i class="icon-picture"></i> Artwork</a></li>
-			<li><a href="/architectures"><i class="icon-road"></i> Architecture</a></li>
-			<li><a href="/exhibitions"><i class="icon-eye-open"></i> Exhibitions</a></li>
-			<li><a href="/publications"><i class="icon-book"></i> Publications</a></li>
+			<li <?php if ($c == 'collections') echo 'class="active"'; ?> >
+				<a href="/collections"><i class="icon-briefcase"></i> Collections</a></li>
+			<li <?php if ($c == 'works') echo 'class="active"'; ?> >
+				<a href="/works"><i class="icon-picture"></i> Artwork</a></li>
+			<li <?php if ($c == 'architectures') echo 'class="active"'; ?> >
+				<a href="/architectures"><i class="icon-road"></i> Architecture</a></li>
+			<li <?php if ($c == 'exhibitions') echo 'class="active"'; ?> >
+				<a href="/exhibitions"><i class="icon-eye-open"></i> Exhibitions</a></li>
+			<li <?php if ($c == 'publications') echo 'class="active"'; ?> >
+				<a href="/publications"><i class="icon-book"></i> Publications</a></li>
 			<li class="divider"></li>
-			<li><a href="/documents"><i class="icon-hdd"></i> Documents</a></li>
+			<li <?php if ($c == 'documents') echo 'class="active"'; ?> >
+				<a href="/documents"><i class="icon-hdd"></i> Documents</a></li>
 			<li class="divider"></li>
-			<li><a href="/users"><i class="icon-user"></i> Users</a></li>
+			<li <?php if ($c == 'users') echo 'class="active"'; ?> >
+				<a href="/users"><i class="icon-user"></i> Users</a></li>
 		</ul>
 	</div>
 
