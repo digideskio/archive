c54b19121b7ebe648e486544b06a4a09fe09ff4c
Christopher Adams <chris@raysend.com>
2012-08-10 11:14:59 -0400

    Check in some temp migration files

---
+++ app/config/db/migrate/_20120809031553_CalculateDocumentDimensions.php
@@ -0,0 +1,40 @@
+<?php
+
+class CalculateDocumentDimensions extends Ruckusing_BaseMigration {
+
+  public function up() {
+  
+  	$target_dir = '';
+  
+  	$result = $this->select_all("SELECT *, documents.id as document_id FROM documents LEFT JOIN formats ON documents.format_id = formats.id WHERE formats.mime_type LIKE 'image%'");
+  	
+  	if($result) {
+  	
+  		foreach($result as $row) {
+  		
+  			$id = $row['document_id'];
+  			$hash = $row['hash'];
+  			$ext = $row['extension'];
+  		
+  			$file = $target_dir . DIRECTORY_SEPARATOR . $hash . '.' . $ext;
+  			
+  			$size = getimagesize($file);
+  			
+  			$width = $size[0];
+  			$height = $size[1];
+  			
+  			echo "$id $hash.$ext $width x $height\n";
+  			
+  			$this->execute("UPDATE documents set width = '$width', height = '$height' WHERE id = '$id'");
+  			
+  		}
+  		
+  	}
+
+  }//up()
+
+  public function down() {
+
+  }//down()
+}
+?>
