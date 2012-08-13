32c466bcb94b2ccf6e7f879c8d7c38c070939571
Christopher Adams <chris@raysend.com>
2012-08-13 02:24:27 -0400

    Don't show year if it is NULL or 0 but missing seconds

--- app/models/Publications.php
+++ app/models/Publications.php
@@ -16,12 +16,15 @@ class Publications extends \lithium\data\Model {
     public function years($entity) {
     
     	$earliest_year = (
-    		$entity->earliest_date != 
-    		'0000-00-00 00:00:00'
+    		$entity->earliest_date != '0000-00-00 00:00:00' &&
+    		$entity->earliest_date != '0000-00-00' &&
+    		$entity->earliest_date != NULL
+    		
     	) ? date_format(date_create($entity->earliest_date), 'Y') : '0';
     	$latest_year = (
-    		$entity->latest_date != 
-    		'0000-00-00 00:00:00'
+    		$entity->latest_date != '0000-00-00 00:00:00' &&
+    		$entity->latest_date != '0000-00-00' &&
+    		$entity->latest_date != NULL
     	) ? date_format(date_create($entity->latest_date), 'Y') : 0;
     	
     	$years = array_unique(array_filter(array($earliest_year, $latest_year)));
