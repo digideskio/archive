175cb10275761c15bd2cdeff2d06228bf94a321a
Christopher Adams <chris@raysend.com>
2012-11-15 11:00:06 -0500

    ditto

--- app/tests/cases/controllers/CollectionsWorksControllerTest.php
+++ app/tests/cases/controllers/CollectionsWorksControllerTest.php
@@ -4,9 +4,18 @@ namespace app\tests\cases\controllers;
 
 use app\controllers\CollectionsWorksController;
 
+use lithium\security\Auth;
+use lithium\storage\Session;
+use lithium\action\Request;
+
 class CollectionsWorksControllerTest extends \lithium\test\Unit {
 
-	public function setUp() {}
+	public function setUp() {
+	
+		Session::config(array(
+			'default' => array('adapter' => 'Php', 'session.name' => 'app')
+		));
+	}
 
 	public function tearDown() {}
 
@@ -15,6 +24,25 @@ class CollectionsWorksControllerTest extends \lithium\test\Unit {
 	public function testAdd() {}
 	public function testEdit() {}
 	public function testDelete() {}
+
+	public function testUnauthorizedAccess() {
+	
+		Auth::clear('default');
+	
+		$this->request = new Request();
+		$this->request->params = array(
+			'controller' => 'collections_works'
+		);
+
+		$collections_works = new CollectionsWorksController(array('request' => $this->request));
+		
+		$response = $collections_works->add();
+		$this->assertEqual($response->headers["Location"], "/login");
+		
+		$response = $collections_works->delete();
+		$this->assertEqual($response->headers["Location"], "/login");
+	
+	}
 }
 
 ?>
