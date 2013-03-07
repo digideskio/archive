2501bcb4825c135e784ab82ce5cf95f234649f74
Christopher Adams <chris@raysend.com>
2013-03-07 08:33:02 -0500

    Add Languages model and test

---
+++ app/models/Languages.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\models;
+
+class Languages extends \lithium\data\Model {
+
+	protected $_meta = array(
+		'key' => 'code'
+	);
+
+	public $validates = array();
+}
+
+?>
---
+++ app/tests/cases/models/LanguagesTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\tests\cases\models;
+
+use app\models\Languages;
+
+class LanguagesTest extends \lithium\test\Unit {
+
+	public function setUp() {}
+
+	public function tearDown() {}
+
+	public function testLanguages() {
+
+		$count = Languages::count();
+
+		$this->assertTrue($count);
+
+		$this->assertEqual('247', $count);
+
+		$english = Languages::find('first', array(
+			'conditions' => array('name' => 'English'),
+		));
+
+		$this->assertEqual('en', $english->code);
+
+		$french = Languages::find('first', array(
+			'conditions' => array('code' => 'fr'),
+		));
+
+		$this->assertEqual('Français', $french->native_name);
+
+	}
+
+}
+
+?>
