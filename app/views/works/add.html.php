ef833fbf497db40b957932c3726d66f8f9b98921
Christopher Adams <chris@raysend.com>
2013-04-26 07:44:25 +0000

    Fix label on work native title

--- app/views/works/add.html.php
+++ app/views/works/add.html.php
@@ -108,7 +108,7 @@ $in_time = $work->in_time ?: date('Y-m-d');
     <?=$this->form->field('artist', array('value' => $artist, 'autocomplete' => 'off', 'data-provide' => 'typeahead', 'data-source' => $artist_names));?>
 	<?=$this->form->field('artist_native_name', array('label' => 'Artist (Native Language)', 'autocomplete' => 'off'));?>
     <?=$this->form->field('title', array('autocomplete' => 'off'));?>
-	<?=$this->form->field('native_name', array('label' => 'Artist (Native Language)', 'autocomplete' => 'off'));?>
+	<?=$this->form->field('native_name', array('label' => 'Title (Native Language)', 'autocomplete' => 'off'));?>
     <?=$this->form->field('earliest_date', array('autocomplete' => 'off'));?>
     <?=$this->form->field('latest_date', array('autocomplete' => 'off'));?>
     <?=$this->form->field('creation_number', array('label' => 'Artwork ID', 'autocomplete' => 'off'));?>
