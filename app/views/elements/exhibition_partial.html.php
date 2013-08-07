4facbf2f143eb27470db98cfa35f338fed704732
Christopher Adams <chris@raysend.com>
2013-08-07 13:21:08 +0000

    Distinguish between artwork and publication components

--- app/views/elements/exhibition_partial.html.php
+++ app/views/elements/exhibition_partial.html.php
@@ -21,13 +21,30 @@
 	
 	<?php 
 	
-		$has_works = isset($exhibition->components[0]->id) ? true : false;
+		$has_components = isset($exhibition->components[0]->id) ? true : false;
 	
-		if ($has_works) { 
-			echo '<span class="label label-success">';
-			echo sizeof($exhibition->components);
-			echo sizeof($exhibition->components) == '1' ? ' Artwork' : ' Artworks';
-			echo '</span>';
+		if ($has_components) { 
+			$work_count = 0;
+			$pub_count = 0;
+
+			foreach ($exhibition->components as $ec) {
+				$work_count =  ($ec->type == 'exhibitions_works') ? $work_count + 1 : $work_count;
+				$pub_count =  ($ec->type == 'exhibitions_publications') ? $pub_count + 1 : $pub_count;
+			}
+			
+			if ($work_count) {
+				echo '<span class="label label-success">';
+				echo $work_count;
+				echo $work_count == '1' ? ' Artwork' : ' Artworks';
+				echo '</span>';
+			}
+			
+			if ($pub_count) {
+				echo '<span class="label label-info">';
+				echo $pub_count;
+				echo $pub_count == '1' ? ' Publication' : ' Publications';
+				echo '</span>';
+			}
 		}
 		
 	?>
