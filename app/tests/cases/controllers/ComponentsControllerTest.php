7e1b1351480f5633a519d95ac5caa7cbd23029ec
Christopher Adams <chris@raysend.com>
2013-08-20 07:12:06 +0000

    Add rules to components

--- app/tests/cases/controllers/ComponentsControllerTest.php
+++ app/tests/cases/controllers/ComponentsControllerTest.php
@@ -15,6 +15,20 @@ class ComponentsControllerTest extends \lithium\test\Unit {
 	public function testAdd() {}
 	public function testEdit() {}
 	public function testDelete() {}
+
+	public function testRules() {
+	
+		$ctrl = new ComponentsController();
+		$rules = isset($ctrl->rules) ? $ctrl->rules : NULL;
+
+		$this->assertTrue(!empty($rules));
+
+		$this->assertEqual(1, sizeof($rules['add']));
+		$this->assertEqual('allowEditorUser', $rules['add'][0]['rule']);
+
+		$this->assertEqual(1, sizeof($rules['delete']));
+		$this->assertEqual('allowEditorUser', $rules['delete'][0]['rule']);
+	}
 }
 
 ?>
