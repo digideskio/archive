82abab2ae8ba7d930bb79ca29a615d8276068e67
Christopher Adams <chris@raysend.com>
2013-09-16 09:00:13 +0000

    Create partial for albums archives editing

--- app/views/works/attachments.html.php
+++ app/views/works/attachments.html.php
@@ -55,39 +55,12 @@ $this->title($work->title);
 
 	<div class="span5">
 
-	<div class="well">
-		<legend>Albums</legend>
-		<table class="table">
-		
-			<?php foreach($albums as $album): ?>
-			<?php $component = $album->components->first(); ?> 
-				<tr>
-					<td>
-						<?=$this->html->link($album->title, $this->url(array('Albums::view', 'slug' => $album->archive->slug))); ?>
-					</td>
-					<td align="right" style="text-align:right">
-			<?=$this->form->create($component, array('url' => $this->url(array('Components::delete', 'id' => $component->id)), 'method' => 'post')); ?>
-			<?=$this->form->submit('Remove', array('class' => 'btn btn-mini btn-danger')); ?>
-			<?=$this->form->end(); ?>
-					</td>
-				</tr>
-			
-			<?php endforeach; ?>
-			
-			<?php if(sizeof($other_albums) > 0): ?>
-			
-			<tr>
-				<td></td>
-				<td align="right" style="text-align:right">
-					<a data-toggle="modal" href="#albumModal" class="btn btn-mini btn-inverse">Add an Album</a>
-				</td>
-			</tr>
-			
-			<?php endif; ?>
-			
-			</table>
-		
-	</div>
+	<?=$this->partial->albums_archives_edit(array(
+		'archive' => $work->archive,
+		'component_type' => 'albums_works',
+		'albums' => $albums,
+		'other_albums' => $other_albums,
+	)); ?>
 	
 	<div class="well">
 		<legend>Exhibitions</legend>
@@ -127,37 +100,6 @@ $this->title($work->title);
 		
 	</div>
 	
-<div class="modal fade hide" id="albumModal">
-	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal">×</button>
-			<h3>Add this Artwork to an Album</h3>
-		</div>
-		<div class="modal-body">
-			<table class="table"><tbody>
-			<?php foreach($other_albums as $oc): ?>
-				<tr>
-					<td>
-						<strong>
-							<?=$this->html->link($oc->title, $this->url(array('Albums::view', 'slug' => $oc->archive->slug))); ?>
-						</strong><br/>
-					</td>
-					<td align="right" style="text-align:right">
-			<?=$this->form->create($oc, array('url' => $this->url(array('Components::add')), 'method' => 'post')); ?>
-			<input type="hidden" name="archive_id1" value="<?=$oc->id ?>" />
-			<input type="hidden" name="archive_id2" value="<?=$work->id ?>" />
-			<input type="hidden" name="type" value="albums_works" />
-			<?=$this->form->submit('Add', array('class' => 'btn btn-mini btn-success')); ?>
-			<?=$this->form->end(); ?>
-					</td>
-				</tr>
-			<?php endforeach; ?>
-			</tbody></table>
-			</div>
-			<div class="modal-footer">
-			<a href="#" class="btn" data-dismiss="modal">Cancel</a>
-	</div>
-</div>
-
 <div class="modal fade hide" id="exhibitionModal">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal">×</button>
