22d6453e1cc5e36fbdc7159bee7e9f6192ce6fb7
Christopher Adams <chris@raysend.com>
2013-09-16 09:09:11 +0000

    Create partial for exhibition archives editing

--- app/views/works/attachments.html.php
+++ app/views/works/attachments.html.php
@@ -62,74 +62,15 @@ $this->title($work->title);
 		'other_albums' => $other_albums,
 	)); ?>
 	
-	<div class="well">
-		<legend>Exhibitions</legend>
-		<table class="table">
-		
-			<?php foreach($exhibitions as $exhibition): ?>
-			<?php $component = $exhibition->components->first(); ?>
-				<tr>
-					<td>
-						<strong><?=$this->html->link($exhibition->title, $this->url(array('Exhibitions::view', 'slug' => $exhibition->archive->slug))); ?></strong>
-						<?php if ($exhibition->archive->years()): ?>
-							<small><?=$exhibition->archive->dates(); ?></small>
-						<?php endif; ?>
-					</td>
-					<td align="right" style="text-align:right">
-			<?=$this->form->create($component, array('url' => $this->url(array('Components::delete', 'id' => $component->id)), 'method' => 'post')); ?>
-			<input type="hidden" name="work_slug" value="<?=$work->archive->slug ?>" />
-			<?=$this->form->submit('Remove', array('class' => 'btn btn-mini btn-danger')); ?>
-			<?=$this->form->end(); ?>
-					</td>
-				</tr>
-			
-			<?php endforeach; ?>
-			
-			<?php if(sizeof($other_exhibitions) > 0): ?>			
-			
-			<tr>
-				<td></td>
-				<td align="right" style="text-align:right">
-					<a data-toggle="modal" href="#exhibitionModal" class="btn btn-mini btn-inverse">Add an Exhibition</a>
-				</td>
-			</tr>
-			
-			<?php endif; ?>
-			
-			</table>
-		
+	<?=$this->partial->exhibitions_archives_edit(array(
+		'archive' => $work->archive,
+		'component_type' => 'exhibitions_works',
+		'exhibitions' => $exhibitions,
+		'other_exhibitions' => $other_exhibitions,
+	)); ?>
+
 	</div>
-	
-<div class="modal fade hide" id="exhibitionModal">
-	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal">×</button>
-			<h3>Add this Artwork to an Exhibition</h3>
-		</div>
-		<div class="modal-body">
-			<table class="table"><tbody>
-			<?php foreach($other_exhibitions as $oe): ?>
-				<tr>
-					<td>
-						<a href="/exhibitions/view/<?=$oe->archive->slug ?>">
-							<strong><?=$oe->title ?></a></strong><br/>
-							<?=$oe->venue ?><br/>
-							<?=$oe->archive->dates() ?>
-					</td>
-					<td align="right" style="text-align:right">
-			<?=$this->form->create($oe, array('url' => $this->url(array('Components::add')), 'method' => 'post')); ?>
-			<input type="hidden" name="archive_id1" value="<?=$oe->id ?>" />
-			<input type="hidden" name="archive_id2" value="<?=$work->id ?>" />
-			<input type="hidden" name="type" value="exhibitions_works" />
-			<?=$this->form->submit('Add', array('class' => 'btn btn-mini btn-success')); ?>
-			<?=$this->form->end(); ?>
-					</td>
-				</tr>
-			<?php endforeach; ?>
-			</tbody></table>
-			</div>
-			<div class="modal-footer">
-			<a href="#" class="btn" data-dismiss="modal">Cancel</a>
-	</div>
+
 </div>
 
 <div class="modal fade hide" id="deleteModal">
@@ -149,8 +90,4 @@ $this->title($work->title);
 			<?=$this->form->submit('Delete', array('class' => 'btn btn-danger')); ?>
 			<?=$this->form->end(); ?>
 	</div>
-</div>
-
-	</div>
-
 </div>
