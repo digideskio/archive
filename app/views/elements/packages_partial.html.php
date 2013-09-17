4fb351667b5e4184d378a4d4fc9dec373d278420
Christopher Adams <chris@raysend.com>
2013-09-17 07:08:08 +0000

    Make partial for packages list

---
+++ app/views/elements/packages_partial.html.php
@@ -0,0 +1,55 @@
+<table class="table table-striped table-bordered">
+<thead>
+	<tr>
+		<th style="width:14px"><i class="icon-eye-close"></i></th>
+		<th>Package</th>
+		<th>Date</th>
+		<?php if($this->authority->canEdit()): ?>
+		<th>Delete</th>
+		<?php endif; ?>
+	</tr>
+</thead>
+
+<?php foreach ($packages as $package): ?>
+
+	<?php
+		$package_date_created = new DateTime($package->date_created);
+
+		if (isset($tz)) {
+			$package_date_created->setTimeZone($tz);
+		}
+		$package_date_display = $package_date_created->format("Y-m-d H:i:s");
+	?>
+
+<tr>
+	
+	<td>
+		<?php
+			$filesystem = $package->filesystem;
+
+			switch ($filesystem) {
+				case "secure":
+					echo '<i class="icon-lock">';
+				break;
+				case "packages":
+					echo '<i class="icon-eye-open">';
+				break;
+			}
+
+		?>
+	</td>
+	<td><?=$this->html->link($package->name, $package->url()); ?></td>
+	<td><?=$package_date_display ?></td>
+	<?php if($this->authority->canEdit()): ?>
+	<td>
+			<?=$this->form->create($package, array('url' => "/packages/delete/$package->id", 'method' => 'post', 'style' => 'margin-bottom:0;')); ?>
+			<?=$this->form->submit('Delete', array('class' => 'btn btn-mini btn-danger')); ?>
+			<?=$this->form->end(); ?>
+	</td>
+	<?php endif; ?>
+
+</tr>
+
+<?php endforeach; ?>
+
+</table>
