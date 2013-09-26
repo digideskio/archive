cea5c782edfcccd68f5be3da76554ea3db03abd3
Christopher Adams <chris@raysend.com>
2013-09-26 06:45:32 +0000

    Show icons on links for each type of archive

--- app/views/elements/links_partial.html.php
+++ app/views/elements/links_partial.html.php
@@ -28,10 +28,53 @@
 					<?=$date ?>
 			</small>
 
-				<?php if (isset($saved) && $link->id == $saved): ?>
-					<span class="label">Saved</span>
+			<?php $has_archives = $link->archives_links->count(); ?>
+
+			<?php if ($has_archives): ?>
+
+				<?php
+					$work_count = 0;
+					$pub_count = 0;
+					$exhibit_count = 0;
+				?>
+
+				<?php foreach ($link->archives_links as $archive_link): ?>
+
+					<?php
+						switch ($archive_link->archive->controller) {
+							case 'works':
+								$work_count++;
+								break;
+							case 'publications':
+								$pub_count++;
+								break;
+							case 'exhibitions':
+								$exhibit_count++;
+								break;
+						}
+					?>
+
+				<?php endforeach; ?>
+	
+				<small style="font-size: smaller;">
+				<?php if ($work_count > 0): ?>
+					<i class="icon-picture"></i>
 				<?php endif; ?>
 
+				<?php if ($pub_count > 0): ?>
+					<i class="icon-book"></i>
+				<?php endif; ?>
+
+				<?php if ($exhibit_count > 0): ?>
+					<i class="icon-eye-open"></i>
+				<?php endif; ?>
+				</small>
+
+			<?php endif; ?>
+
+			<?php if (isset($saved) && $link->id == $saved): ?>
+				<span class="label">Saved</span>
+			<?php endif; ?>
 		</p>
 
 		<p>
