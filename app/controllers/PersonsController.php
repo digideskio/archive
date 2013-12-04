d7d50401e9a71d34bc3a10a8f0118db44e50f5e6
Christopher Adams <chris@raysend.com>
2013-12-04 05:35:12 -0500

    Write PersonsController rules and test

--- app/controllers/PersonsController.php
+++ app/controllers/PersonsController.php
@@ -4,6 +4,24 @@ namespace app\controllers;
 
 class PersonsController extends \lithium\action\Controller {
 
+	public $rules = array(
+		'index' => array(
+			array('rule' => 'allowAnyUser', 'redirect' => "Sessions::add"),
+		),
+		'view' => array(
+			array('rule' => 'allowAnyUser', 'redirect' => "Sessions::add"),
+		),
+		'add' => array(
+			array('rule' => 'allowEditorUser', 'redirect' => "Pages::home"),
+		),
+		'edit' => array(
+			array('rule' => 'allowEditorUser', 'redirect' => "Pages::home"),
+		),
+		'delete' => array(
+			array('rule' => 'allowEditorUser', 'redirect' => "Pages::home"),
+		),
+	);
+
 	public function index() {
 
 	}
