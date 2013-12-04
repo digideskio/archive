279e138cdb93fca75c1031e6f768679e7857adee
Christopher Adams <chris@raysend.com>
2013-12-04 06:20:52 -0500

    Write breadcrumbs partial

---
+++ app/views/elements/breadcrumbs_partial.html.php
@@ -0,0 +1,29 @@
+<div id="location" class="row-fluid">
+    
+	<ul class="breadcrumb">
+
+	<?php for($i = 0; $i < sizeof($crumbs); $i++): ?>
+
+		<?php $crumb = $crumbs[$i]; ?>
+
+		<?php if (isset($crumb['active']) && $crumb['active'] == true): ?>
+		<li class="active">
+		<?php else: ?>
+		<li>
+		<?php endif; ?>
+			<?php if (isset($crumb['url']) && !empty($crumb['url'])): ?>
+				<?=$this->html->link($crumb['title'], $crumb['url']); ?>
+			<?php else: ?>
+				<?=$crumb['title'] ?>
+			<?php endif; ?>
+
+			<?php if ($i < sizeof($crumbs) - 1): ?>
+				<span class="divider">/</span>
+			<?php endif; ?>
+		</li>
+
+	<?php endfor; ?>
+
+	</ul>
+
+</div>
