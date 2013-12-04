a61984da2222b165ac8500bd103b355607e284f4
Christopher Adams <chris@raysend.com>
2013-12-04 05:32:38 -0500

    Add stubs for artists routes, persons controller, views

---
+++ app/controllers/PersonsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\controllers;
+
+class PersonsController extends \lithium\action\Controller {
+
+	public function index() {
+
+	}
+
+	public function add() {
+
+	}
+
+	public function view() {
+
+	}
+
+	public function edit() {
+
+	}
+
+	public function delete() {
+
+		return $this->redirect('Persons::index');
+	}
+
+}
+
+?>
