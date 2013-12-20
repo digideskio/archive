a5d44c0fd12e70c5eeac9c8e1e516d63400807a5
Christopher Adams <chris@raysend.com>
2013-12-20 06:51:30 -0500

    Set created and modified dates for Components

--- app/models/Components.php
+++ app/models/Components.php
@@ -26,4 +26,27 @@ class Components extends \lithium\data\Model {
 
 }
 
+/**
+ * Date Modified Filter
+ */
+
+Components::applyFilter('save', function($self, $params, $chain) {
+	// Custom pre-dispatch logic goes here
+	date_default_timezone_set('UTC');
+
+	// Check if this is a new record
+	if(!$params['entity']->exists()) {
+
+		// Set the date created
+		$params['data']['date_created'] = date("Y-m-d H:i:s");
+
+	}
+
+	// Set the date modified
+	$params['data']['date_modified'] = date("Y-m-d H:i:s");
+
+	return $chain->next($self, $params, $chain);
+
+});
+
 ?>
--- app/tests/cases/models/ComponentsTest.php
+++ app/tests/cases/models/ComponentsTest.php
@@ -3,12 +3,34 @@
 namespace app\tests\cases\models;
 
 use app\models\Components;
+use app\models\ComponentsHistories;
 
 class ComponentsTest extends \lithium\test\Unit {
 
 	public function setUp() {}
 
-	public function tearDown() {}
+	public function tearDown() {
+	
+		Components::all()->delete();
+		ComponentsHistories::all()->delete();
+
+	}
+
+	public function testDateCreatedModifiedFilter() {
+
+		$component = Components::create();
+		$component->save(array(
+			'archive_id1' => 1,
+			'archive_id2' =>2
+		));
+
+		$this->assertTrue(strtotime($component->date_created) != false);
+		$this->assertTrue(strtotime($component->date_modified) != false);
+
+		$this->assertTrue($component->date_created != '0000-00-00 00:00:00');
+		$this->assertTrue($component->date_modified != '0000-00-00 00:00:00');
+
+	}
 
 
 }
