dbd8524afe38601b3425eea1dad3e3247621f014
Christopher Adams <chris@raysend.com>
2014-01-23 21:34:41 -0500

    Make all users active for migration

--- app/config/db/migrate/20140123225422_AddActiveColumnToUsers.php
+++ app/config/db/migrate/20140123225422_AddActiveColumnToUsers.php
@@ -5,6 +5,8 @@ class AddActiveColumnToUsers extends Ruckusing_Migration_Base
     public function up()
     {
 		$this->add_column("users", "active", "boolean", array("null" => false));
+
+		$this->execute("UPDATE users SET active = '1'");
     }//up()
 
     public function down()
