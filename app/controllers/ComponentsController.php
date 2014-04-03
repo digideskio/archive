866658b97f84f01396b9c6101092315432576948
Christopher Adams <chris@raysend.com>
2014-04-03 16:09:56 +0800

    Test ComponentsController::add()

--- app/controllers/ComponentsController.php
+++ app/controllers/ComponentsController.php
@@ -25,19 +25,24 @@ class ComponentsController extends \lithium\action\Controller {
 
 		if ($this->request->data) {
 
-			$archive_id1 = $this->request->data['archive_id1'];
-			$archive_id2 = $this->request->data['archive_id2'];
+            $data = $this->request->data;
 
-			$component = Components::find('first', array(
-				'conditions' => compact('archive_id1', 'archive_id2'),
-			));
+			$archive_id1 = isset($data['archive_id1']) ? $data['archive_id1'] : null;
+			$archive_id2 = isset($data['archive_id2']) ? $data['archive_id2'] : null;
 
-			if (!$component) {
+            if($archive_id1 && $archive_id2) {
 
-				$component = Components::create();
-				$component->save($this->request->data);
+                $component = Components::find('first', array(
+                    'conditions' => compact('archive_id1', 'archive_id2'),
+                ));
 
-			}
+                if (!$component) {
+
+                    $component = Components::create();
+                    $component->save($this->request->data);
+
+                }
+            }
 		}
 
 		return $this->redirect($this->request->env('HTTP_REFERER'));
--- app/tests/cases/controllers/ComponentsControllerTest.php
+++ app/tests/cases/controllers/ComponentsControllerTest.php
@@ -4,15 +4,46 @@ namespace app\tests\cases\controllers;
 
 use app\controllers\ComponentsController;
 
-class ComponentsControllerTest extends \lithium\test\Unit {
+use app\models\Components;
+use app\models\ComponentsHistories;
+
+class ComponentsControllerTest extends \li3_unit\test\ControllerUnit {
+
+	public $controller = 'app\controllers\ComponentsController';
 
 	public function setUp() {}
 
-	public function tearDown() {}
+    public function tearDown() {
+        Components::all()->delete();
+        ComponentsHistories::all()->delete();
+    }
 
 	public function testIndex() {}
 	public function testView() {}
-	public function testAdd() {}
+
+	public function testAdd() {
+		// Test that this action processes and saves the correct data, namely
+		// a component
+        $component_data = array(
+            'archive_id1' => '1',
+            'archive_id2' => '2',
+            'type' => 'albums_works'
+        );
+		$data = $this->call('add', array(
+            'data' => $component_data
+		));
+
+        // Test that the controller returns a redirect response
+        $this->assertTrue(!empty($data->status) && $data->status['code'] == 302);
+
+		// Look up the objects that were saved
+        $component = Components::find('first', array(
+            'conditions' => $component_data
+        ));
+
+        $this->assertTrue(!empty($component));
+    }
+
 	public function testEdit() {}
 	public function testDelete() {}
 
